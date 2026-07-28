4bfa1515586a6bdfbb5ea18273082cc376995b37
Fatihsen96 <mustafafatihsen@gmail.com>
2026-07-28 15:43:26 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://bist-terminal-hds9.onrender.com/api/:path*',
+      },
+    ];
+  },
+};
 
+export default nextConfig;
