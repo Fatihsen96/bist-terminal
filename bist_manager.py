4b5790cfce8b7b5ce1380cbfc63d016f5ea3f226
Fatihsen96 <mustafafatihsen@gmail.com>
2026-07-28 14:11:08 +0300

    Update bist_manager.py

--- bist_manager.py
+++ bist_manager.py
@@ -1,1 +1,11 @@
+from bist_manager import BISTUniverseManager
 
+# Servisi başlatın
+universe_mgr = BISTUniverseManager()
+
+# Canlı hisse listesini çekin
+live_symbols = universe_mgr.fetch_live_universe()
+
+# Örnek: Bilinen sembollerle yeni IPO'ları tespit edin
+known_symbols = {"THYAO", "GARAN", "ASELS"}  # Veritabanınızdaki mevcut liste
+new_ipos = universe_mgr.detect_new_ipos(live_symbols, known_symbols)
